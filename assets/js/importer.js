537b91ad6b4768c1efd24434b12508286e7574be
Joe <joedpreece@gmail.com>
2023-01-31 20:33:26 +0000

    Test

---
+++ assets/js/importer.js
@@ -0,0 +1,13 @@
+console.log(window.location.href)
+
+$(function(){
+    $("#header").load("/assets/html/header.html");
+});
+
+$(function(){
+    $("#footer").load("/assets/html/footer.html");
+});
+
+$(function(){
+    $("#menu").load("/assets/html/menu.html");
+});
